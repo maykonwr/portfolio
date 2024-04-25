45b46e43f1e47da43919ebc48fa7356e93e6b909
MaykonWr <maykonsilva60@gmail.com>
2024-04-25 17:36:13 -0300

    Fix redirect

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -44,7 +44,7 @@ function NavBar() {
       className={navColour ? "sticky" : "navbar"}
     >
       <Container>
-        <Navbar.Brand href="/" className="d-flex">
+        <Navbar.Brand href="https://maykonwr.github.io/portfolio/" className="d-flex">
           <img src={logo} className="img-fluid logo" alt="brand" />
         </Navbar.Brand>
         <Navbar.Toggle
