57b70d3e101148c14c288164836bfcc210f464a0
MaykonWr <maykonsilva60@gmail.com>
2025-12-25 23:24:21 -0300

    Remove old imports

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Card from "react-bootstrap/Card";
 import { ImPointRight } from "react-icons/im";
 
